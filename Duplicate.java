9c00beef2d30a2d27fa86e72d49581481d66581e
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-02-23 19:40:25 +0530

    Add files via upload

---
+++ Duplicate.java
@@ -0,0 +1,32 @@
+class  Duplicate
+{
+	public static void main(String[] args) 
+	{
+		
+		int a[]={8,2,2,6,6,15};
+	    for(int i=0;i<=(a.length)-1;i++)
+		   {
+			 for(int j=i+1;j<(a.length)-1;j++)
+			  {
+				if(a[i]==a[j])
+				{
+					
+				  System.out.println("Duplicate values are"+a[i]);
+				  break;
+				}
+			  }
+		   }
+			  
+	
+				
+
+	
+
+
+	
+
+	}
+}
+
+
+
