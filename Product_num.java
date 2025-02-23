cd3e8a3b731c3195019d05b4e343f539d654fbd4
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-02-23 19:51:24 +0530

    Add files via upload

---
+++ Product_num.java
@@ -0,0 +1,13 @@
+class Product_num 
+{
+	public static void main(String[] args) 
+	{
+		int product=1;
+		for(int i=-21;i<=-12;i++)
+		{
+			product=product*i;
+
+		}
+		System.out.println("The product is"+product);
+	}
+}
