cdae0b189cd44ee08369f7cebe03ef4d2b6c5606
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-02-24 21:08:30 +0530

    Add files via upload

---
+++ oddnum.java
@@ -0,0 +1,14 @@
+class oddnum 
+{
+	public static void main(String[] args) 
+	{
+		for(char i='a';i<='z';i++)
+		
+		{
+		 int result=i;
+		 if(result%2!=0)
+         System.out.println(result);
+		}
+		
+	}
+}
