fa0ecfafeca3e864dd74d2a50b15b9ce268a1602
AkhilaDavulapally <davulapallyakhila@gmail.com>
2025-02-28 20:53:32 +0530

    Add files via upload

---
+++ Match.java
@@ -0,0 +1,17 @@
+class Match 
+{
+	public static void main(String[] args) 
+	{
+		String s="z141562@";
+		String s1="z141562@";
+		if(s==s1)
+		{
+			System.out.println("Matched");
+		}
+		else
+		{
+            System.out.println("Not matched");
+		}
+	}	
+	
+}
